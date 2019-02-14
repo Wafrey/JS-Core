20bb7b743d1625d7bf8550a81e6c9137fd67f62f
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-14 23:10:25 +0200

    Add files via upload

---
+++ Document object model - More Exercise/Order-the-Names/solution.js
@@ -0,0 +1,21 @@
+function solve() {
+    let buttonElement = document.querySelector('button');
+    buttonElement.addEventListener('click', clickEvent);
+
+    function clickEvent() {
+        let input = document.querySelector('input');
+        let firstLetterIndex = input.value.toLowerCase().charCodeAt(0) - 97;
+        if (firstLetterIndex >= 0 && firstLetterIndex <= 25) {
+            name = input.value.charAt(0).toUpperCase() + input.value.substr(1).toLowerCase();
+            let liElements = document.querySelectorAll('li');
+            let li = liElements[firstLetterIndex];
+            if (li.textContent === "") {
+                li.textContent += name;
+            } else {
+                li.textContent += ", " + name;
+            }
+
+            document.querySelector('input').value = '';
+        }
+    }
+}
