0f724c29eeb0630eb1ae3d336b95c2b12d42c340
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-16 13:37:17 +0200

    Add files via upload

---
+++ Advanced Functions - Exercise/Functional Sum/problem3.js
@@ -0,0 +1,18 @@
+let add = (function () {
+    
+    let sum = 0;
+
+    function add(number) {
+        sum += number;
+       return add;
+    
+    }
+    
+    add.toString=function(){
+        return sum;
+    }
+
+    return add;
+})()
+
+add(1);
