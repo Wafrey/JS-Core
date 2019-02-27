78880ede733842007d785060b5caa7d38c36dff2
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-02-27 19:02:25 +0200

    Add files via upload

---
+++ jQuery- LAB/05. Countries Table/initialize-table.js
@@ -0,0 +1,80 @@
+function initializeTable() {
+    $('#createLink').on('click', addCountry);
+
+    fixLinks();
+
+    function addCountry() {
+
+        let country = $('#newCountryText').val();
+        let capital = $('#newCapitalText').val();
+
+        if (capital !== '' && country !== '') {
+            createCountry(country, capital);
+        }
+
+        fixLinks();
+    }
+
+    function createCountry(country, capital) {
+        let table = $('#countriesTable');
+
+        let row = $('<tr>');
+        let CountryAndCapital = $(`<td>${country}</td><td>${capital}</td>`);
+
+
+
+        let actions = $('<td>')
+            .append($('<a href="#">[Up]</a>')
+                .click(moveUp))
+            .append($('<a href="#">[Down]</a>')
+                .click(moveDown))
+            .append($('<a href="#">[Delete]</a>')
+                .click(deleteRow));
+
+
+        row.append(CountryAndCapital, actions)
+
+        table.append(row);
+
+
+    }
+
+    function moveUp() {
+        let currentRow = $(this).parent().parent();
+
+        currentRow.insertBefore(currentRow.prev());
+
+        fixLinks();
+    }
+
+    function moveDown() {
+        let currentRow = $(this).parent().parent();
+
+        currentRow.insertAfter(currentRow.next());
+
+        fixLinks();
+    }
+
+    function deleteRow() {
+        let currentRow = $(this).parent().parent();
+
+        currentRow.remove();
+
+        fixLinks();
+    }
+
+    function fixLinks() {
+
+        $('a').show();
+
+        $('#countriesTable tr:nth-child(3) td:nth-child(3) a:first-child').hide();
+        $('#countriesTable tr:last-child td:nth-child(3) a:nth-child(2)').hide();
+    }
+
+
+    createCountry('Bulgaria', 'Sofia');
+    createCountry('Germany', 'Berlin');
+    createCountry('Russia', 'Moscow');
+
+    fixLinks();
+}
