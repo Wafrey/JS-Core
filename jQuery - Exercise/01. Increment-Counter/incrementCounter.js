c3fe351e47b93fcb498176438e374cf0b75f559e
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-02 18:52:28 +0200

    Add files via upload

---
+++ jQuery - Exercise/01. Increment-Counter/incrementCounter.js
@@ -0,0 +1,43 @@
+function increment(selector) {
+    let container = $(selector);
+    let fragment = document.createDocumentFragment();
+    let textArea = $('<textarea>');
+    let incrementBtn = $('<button>Increment</button>');
+    let addBtn = $('<button>Add</button>');
+    let list = $('<ul>');
+
+    // Textarea formation
+
+    textArea.val(0);
+    textArea.addClass('counter');
+    textArea.attr('disable', true);
+
+    //Buttons formation
+
+    incrementBtn.addClass('btn');
+    incrementBtn.attr('id', 'incrementBtn');
+    addBtn.addClass('btn');
+    addBtn.attr('id', 'addBtn');
+
+    // List Formation
+
+    list.addClass('results');
+
+    // Events 
+
+    $(incrementBtn).on('click', function () {
+        textArea.val(+textArea.val() + 1)
+    });
+
+    $(addBtn).on('click', function () {
+
+        let li = $(`<li>${textArea.val()}</li>`);
+        li.appendTo(list);
+    });
+
+    textArea.appendTo(fragment);
+    incrementBtn.appendTo(fragment);
+    addBtn.appendTo(fragment);
+    list.appendTo(fragment);
+    container.append(fragment);
+}
