798de46cef293750890ebe0b07981e39d36d5c90
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-07 13:37:23 +0200

    Add files via upload

---
+++ More Exercise JavaScript Syntax and Operators/problem5.js
@@ -0,0 +1,17 @@
+function solve(amount, coinValues) {
+    let coins = coinValues.sort((a, b) => b - a);
+    let result = [];
+    for (i = 0; i < coins.length; i++) {
+      if (amount - coins[i] >= 0) {
+        result.push(coins[i]);
+        amount -= coins[i];
+        //console.log(`Added coin ${coins[i]}, amount ${amount}, i = ${i}`);
+        i = -1;
+      } else {
+      }
+    }
+    console.log(result.join(", "));
+  }
+  
+  solve(46, [10, 25, 5, 1, 2]);
+  
