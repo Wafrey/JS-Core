a4e3d728da248cd3864e92ec7d7ab34c9cbeb94a
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-03-17 19:20:04 +0200

    Add files via upload

---
+++ JavaScript Syntax and Operators - Exercise/problem8.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+    let [input, town, time, flightNum, gateNum] = arr;
+
+    console.log(`${input}: Destination - ${town}, Flight - ${flightNum}, Time - ${time}, Gate - ${gateNum}`);
+
+}
+
+solve(['Departures', 'London', '22:45', 'BR117', '42']);
