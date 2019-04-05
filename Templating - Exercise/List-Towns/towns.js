6895ae39f26659d1ad9848347c7ae7f6383216ad
Georgi Dimitrov <43094653+Wafrey@users.noreply.github.com>
2019-04-05 13:27:50 +0300

    Add files via upload

---
+++ Templating - Exercise/List-Towns/towns.js
@@ -0,0 +1,24 @@
+function attachEvents(){
+
+    $('#btnLoadTowns').on('click', getTowns)
+
+    function getTowns (){
+
+        let towns = $('#towns').val().split(', ');
+        
+        let template = $('#towns-template').html();
+        let compiledTemplate = Handlebars.compile(template);
+
+        let context={
+
+            towns: towns          
+        }
+
+        $('#root').html(compiledTemplate(context));
+
+        // get template
+        // compile template
+        // create context
+        // add to html
+    }  
+}
